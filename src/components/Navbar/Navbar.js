212ba80517144bf190346c13f78b6c1bb44d8fa8
Lope Ariyo <lopeariyo@gmail.com>
2020-03-30 13:57:48 +0100

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const navbar = props => (
+    <header>
+        <nav>
+            <div>
+                <a href="www.lopeariyo.com"> LOPE ARIYO </a>
+            </div>
+            <div>
+                <ul></ul>
+            </div>
+            <div></div>
+        </nav>
+    </header>
+);
