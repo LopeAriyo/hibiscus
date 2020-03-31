a756f24f488dbe8fe781d62632b565bbe3897e31
Lope Ariyo <lopeariyo@gmail.com>
2020-03-31 08:20:36 +0100

    Started Navigation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Navbar from "./components/Navbar/Navbar";
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Navbar />
+                <main style={{ marginTop: "75px" }}>
+                    <h4>
+                        Hibiscus: Discover Fresh Flavours from West Africa with
+                        the Observer Rising Star of Food 2017{" "}
+                    </h4>
+                    <p>
+                        Nigerian-inspired flavours and cooking methods can
+                        transform everyday dishes into something spectacular.
+                        Whether it’s experimenting with new ingredients
+                        (Hibiscus Chicken), reimagining old favourites
+                        (Grapefruit and Guava Cheesecake; Baked Kuli Kuli Cod
+                        with Cayenne Yam Chips; Lagos Mess), exploring different
+                        techniques (Cheat’s Ogi, Chin Chin) or finding
+                        alternatives to everyday staples (Plantain Mash with
+                        Ginger, Corn and Okra Gravy; Nigerian Roast Veg), Lopè
+                        will help you discover all that modern Nigerian food has
+                        to offer.
+                    </p>
+                </main>
+                <footer className="footer"></footer>
+            </div>
+        );
+    }
 }
 
 export default App;
