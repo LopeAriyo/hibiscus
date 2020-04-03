06e8f70d745f099233abdba189c2abb2107ab32e
Lope Ariyo <lopeariyo@gmail.com>
2020-04-03 12:40:04 +0100

    Added static content for Contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,7 +3,10 @@ import "./Contact.css";
 
 const contact = () => (
     <div className="contact-card">
-        <h4>Literary Agent</h4>
+        <h1>Contact</h1>
+
+        <h2>Literary Agent</h2>
+        <p>For any literary or collaboration inquiries please email my agent</p>
         <h3>Charlie Brotherstone</h3>
         <p>charlie@bcm-agency.com </p>
     </div>
